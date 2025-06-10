f705be74f3e1e99c92f27b1c0c6fa60903f2e461
caleb <cakpomughe@gmail.com>
2025-06-10 17:06:54 +0100

    search input dark mode , dialog backdrop change

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -40,7 +40,7 @@ export const SearchInput = () => {
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
             onClick={handleInputClick}
-            className="pl-10 pr-10 bg-gray-50 border-none rounded-lg h-10 w-[400px] max-w-md focus:ring-2 focus:ring-gray-200 text-gray-500"
+            className="pl-10 pr-10 bg-[#F8F8F8] dark:bg-background border-none rounded-lg h-10 w-[400px] max-w-md focus:ring-2 focus:ring-gray-200 text-gray-500"
           />
           <Search className="absolute right-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-gray-400" />
         </form>
